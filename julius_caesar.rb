310a2b954efa310e0e803b33ef64e541f4ef2c80
Chongsawad Saiaram <Chongsawad252@msn.com>
2010-11-19 03:06:11 +0700

    ShiftWheelAdded

--- julius_caesar.rb
+++ julius_caesar.rb
@@ -1,20 +1,25 @@
 class JuliusCaesar
-  attr_accessor :c, :ck, :m, :mk, :clk, :sh
-  CLKWISE = 1
-  COUNTERCLKWISE = 2
+  attr_accessor :ck, :m, :mk, :clk, :sh, :map
+  CLOCKWISE = "R"
+  COUNTERCLOCKWISE = "L"
   
-  def initialize
+  def initialize(m,sh,clk)
+    @m = m == nil ? "A page of history is worth a volume of logic." : m
+    @m = @m.downcase.gsub(/[^\w]/,"")
+    
     @ck = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    @map = {}
     @mk = Array.new(@ck)
     @mk.collect! { |mk| mk.downcase }
-    @clk = CLOCKWISE
-    @sh = 1
+    
+    @clk = clk == nil ? CLOCKWISE : clk
+    @sh = sh.to_i == nil ? 0 : sh.to_i
     
     self.shiftCipherWheel
   end
   
   def shiftCipherWheel
-    s="A page of history is worth a volume of logic."
+    
     if @clk == CLOCKWISE
       @sh.times do
         t = @ck.shift
@@ -26,13 +31,21 @@ class JuliusCaesar
         @ck = @ck.unshift(t)
       end
     end
-      
-    print("Plaintext \t: #{@mk*','}\nCipherText \t: #{@ck*','}\n")
+
+    print("Letters \t: #{@mk*','}\nCipherKeys \t: #{@ck*','}\n")
+    
+    26.times do |i|
+      @map[@mk[i]] = @ck[i]
+    end
+    print "Plaintext\t: #{@m}\n"
+    print "Ciphertext\t: "
+    @m.each_char { |ss| print "#{@map[ss]}" }
+    puts "\n"
   end
   
 end
 
-jj = JuliusCaesar.new()
+jj = JuliusCaesar.new(ARGV[0],ARGV[1],ARGV[2])
 
 
 
